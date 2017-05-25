b11a5723dc80a543d51a2bf06cdbfdd1f7c8e7dc
Rajeshadepu <asingh@ctepl.com>
2017-05-25 11:50:11 +0530

    removed unused code

--- public/pages/common/common.service.js
+++ public/pages/common/common.service.js
@@ -1,13 +1,13 @@
 var eknock=eknock||angular.module('eknock');
 
-eknock.factory('$exceptionHandler', ['$log',function($log, logErrorsToBackend) {
+eknock.factory('$exceptionHandler', ['$log',function($log) {
     return function myExceptionHandler(exception, cause) {
      // logErrorsToBackend(exception, cause);
       $log.warn(exception, cause);
     };
   }])
 
-eknock.service('commonDataHolder', ['$log',function ($log, logErrorsToBackend) {
+eknock.service('commonDataHolder', ['$log',function ($log) {
       this.holdData={};
 }]);
 
