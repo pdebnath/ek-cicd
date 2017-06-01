2237492cda5ce50c72256587138d998a3bd7755e
rvelugu@ctepl.com <rvelugu@ctepl.com>
2017-06-01 18:58:17 +0530

    Renamed files

--- public/pages/common/HeaderController.js
+++
@@ -1,4 +0,0 @@
-var eknock=eknock||angular.module('eknock');
-eknock.controller('HeaderController',['$rootScope','$scope','$state','HeaderFactory',function($rootScope,$scope,$state,HeaderFactory){
-
-}]);
--- public/pages/common/HeaderFactory.js
+++
@@ -1,7 +0,0 @@
-var eknock=eknock||angular.module('eknock');
-
-eknock.factory('HeaderFactory',['$http','$log','$q',function($http,$log,$q){
-	return{
-        	
-        }
-}])
