791d248ce7d01a989b8c6c219524693cc5a9bdde
rvelugu@ctepl.com <rvelugu@ctepl.com>
2017-06-05 17:31:57 +0530

    UI changes

--- routes/dashborad/HomeOwnerDashBoardController.js
+++ routes/dashborad/HomeOwnerDashBoardController.js
@@ -6,13 +6,15 @@ var logger = require('./../../config/logger');
   /* Below code is used for getting claimed properties details)*/
   router.post('/getClaimedProperties', function(req, res) {
         excuteQuery(function(conn,err){
-          logger.info(req.body.userId);
+          logger.info('getClaimedProperties POST Request Started');
+          logger.info('call sp_select_homeowner_claimed_properties(%s)',req.body.userId);
           conn.query('call sp_select_homeowner_claimed_properties(?);',[req.body.userId],function(err,rows){
             conn.release();
             if(err){
               logger.error('Exception while fetching claimed properties :'+err);
               return res.json({status:0,resp:'Exception while fetching claimed properties :'+err}) 
             }
+            logger.info('getClaimedProperties POST Request Ended');
             return  res.json({status:1,resp:rows[0]});
           });
         })
@@ -20,13 +22,15 @@ var logger = require('./../../config/logger');
   /* Below code is used to fetch deal conversions for particular property)*/
   router.post('/getClaimedPropertiesDeals', function(req, res) {
         excuteQuery(function(conn,err){
-          logger.info(req.body.userId);
+          logger.info('getClaimedPropertiesDeals POST Request Started');
+          logger.info('call sp_select_home_owner_deals(%s)',req.body.propertyId);
           conn.query('call sp_select_home_owner_deals(?);',[req.body.propertyId],function(err,rows){
             conn.release();
             if(err){
               logger.error('Exception while fetching claimed property deals :'+err);
               return res.json({status:0,resp:'Exception while fetching claimed property deals :'+err}) 
             }
+            logger.info('getClaimedPropertiesDeals POST Request Ended');
             return  res.json({status:1,resp:rows[0]});
           });
         })
