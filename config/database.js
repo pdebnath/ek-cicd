59adf23cbef4b144205d8a348faa6e4dd9bfcf05
Srikanth Tirukovalluru <stirukovalluru@ctepl.com>
2017-06-07 16:21:32 +0530

    Added QA db details

--- config/database.js
+++ config/database.js
@@ -2,6 +2,6 @@ module.exports = {
         host: 'ek-dev-db-instance-cluster.cluster-cwmb3qfdlgqv.us-east-1.rds.amazonaws.com',
         user: 'ekdevdbadmin',
         password: 'Ea$yKn0ck$786',
-        database: 'ekdevdb',
+        database: 'ekqadb',
         connections:100,
 };
