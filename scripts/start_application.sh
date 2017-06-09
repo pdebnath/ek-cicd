23238673dc2b95722e1eb3605f38ef4d134de917
Padmanava Debnath <pdebnath@ctepl.com>
2017-06-09 16:13:04 +0530

    permissions in appspec

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-EASYKNOCK_HOME='/home/ec2-user/ek-cicd'
+EASYKNOCK_APP_DIR='/home/ec2-user/ek-cicd'
 
 TEMP_STAGING_DIR='/tmp/codedeploy-deployment-staging-area'
 
-# Copy the WAR file to the webapps directory
-su ec2-user -c 'cp -R '$TEMP_STAGING_DIR'/* '$EASYKNOCK_HOME
+# Copy the files in staging dir recursively to easyknock app directory
+cp -R $TEMP_STAGING_DIR/* $EASYKNOCK_APP_DIR
